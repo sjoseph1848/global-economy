5a8c3621691dfd2c69659c7e83cb8ab1dedfa8dc
sjoseph1848 <sjoseph1848@gmail.com>
2019-08-23 06:39:32 -0500

    got correct sizing of the chart

--- src/app/bottom-section/debt/debt.component.ts
+++ src/app/bottom-section/debt/debt.component.ts
@@ -14,11 +14,11 @@ export class DebtComponent implements OnInit {
   };
 
   public barChartLabels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-  public barChartType = 'bar';
+  public barChartType = 'line';
   public barChartLegend = true;
   public barChartData = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
+    {data: [65, 59, 80, 81, 56, 55, 40], label: 'US'},
+    {data: [28, 48, 40, 19, 86, 27, 90], label: 'EU'}
   ];
 
   ngOnInit() {
